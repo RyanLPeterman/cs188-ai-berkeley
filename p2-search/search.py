df42fa9c51a319f48808c083178ca677673edc6b
Ryan Peterman <ryanlpeterman@gmail.com>
2016-06-18 00:57:28 -0400

    Finished dfs

--- p2-search/search.py
+++ p2-search/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -86,17 +86,77 @@ def depthFirstSearch(problem):
     print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    # list of actions leading to goal
+    actions = []
+    visited_set = set()
+
+    stack = util.Stack()
+    stack.push((problem.getStartState(), None, 1))
+
+    while not stack.isEmpty():
+
+        curr = stack.pop()
+        visited_set.add(curr[0])
+        if curr[1]:
+            actions.append(curr[1])
+
+        # import sys
+        # sys.stdin.readline()
+        # print "Curr:", curr[0]
+        # print "Curr's successors:", problem.getSuccessors(curr[0])
+        # print "Stack: ", stack._print()
+        # print curr[1]
+
+        # Done
+        if problem.isGoalState(curr[0]):
+            return actions
+
+
+        # Dead End
+        is_dead_end = True
+
+        # Process all potential nodes
+        for node in problem.getSuccessors(curr[0]):
+            if not node[0] in visited_set:
+                is_dead_end = False
+                stack.push(node)
+
+        if is_dead_end:
+            # Pop actions until state is restored to last node on stack
+            curr_state = curr[0]
+            goal_state = stack.top()[0]
+
+            while goal_state not in [coord[0] for coord in problem.getSuccessors(curr_state)]:
+                prev_action = actions.pop()
+                delta_x = 0
+                delta_y = 0
+
+                if prev_action == 'North':
+                    delta_y = -1
+                elif prev_action == 'South':
+                    delta_y = 1
+                elif prev_action == 'East':
+                    delta_x = -1
+                elif prev_action == 'West':
+                    delta_x = 1
+                else:
+                    raise Exception('Invalid Direction Found')
+
+                curr_state = (int(curr_state[0]) + delta_x, int(curr_state[1]) + delta_y)
+
+
+    print "Exausted search space, but found no goal"
+    return actions
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
+    priority_queue = util.PriorityQueue()
+
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
